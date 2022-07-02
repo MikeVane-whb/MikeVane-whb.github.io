ca690a5c82bc8438e1ce1ce74cd3bd42da089394
MikeVane-whb <1312706383@qq.com>
2022-07-02 20:45:20 +0800

    Site updated: 2022-07-02 20:45:20

--- js/matery.js
+++ js/matery.js
@@ -100,11 +100,11 @@ $(function () {
                 this.insertAdjacentElement('afterend', captionDiv)
             }
         });
-        $('#articleContent, #myGallery').lightGallery({
-            selector: '.img-item',
-            // 启用字幕
-            subHtmlSelectorRelative: true
-        });
+        // $('#articleContent, #myGallery').lightGallery({
+        //     selector: '.img-item',
+        //     // 启用字幕
+        //     subHtmlSelectorRelative: false
+        // });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
